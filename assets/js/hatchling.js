dcfdb28d84a09ecc998d4e1a2a7cbdf6f705577c
Cherry Wang <cherryregister@gmail.com>
2015-11-06 19:30:06 +0000

    added content json

--- assets/js/hatchling.js
+++ assets/js/hatchling.js
@@ -6,10 +6,13 @@
 
     //controllers
     app.controller('ContentController', function($scope, $http) {
+        $scope.test = "Angular is working!";
         $scope.content = {};
+        $scope.repeatlist = [];
         $http.get('data/content.json').success(function(data){
             $scope.content = data[0];
-        })
+            $scope.repeatlist = data[0].products;
+        });
     });
 
 })();
